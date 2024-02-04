797b9df432138029095e94a55e313eb138fc2e01
Whyhere <148620619+Whyherere@users.noreply.github.com>
2024-02-04 17:51:52 +0300

    Add files via upload

---
+++ add/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <Chec 
+  :dela="dela" 
+  @delit="delit" 
+  :newDelas="newDelas" 
+  @addDela="addDela" 
+  />
+</template>
+<script>
+import Chec from './components/Chec.vue'
+export default {
+  name: 'App',
+  components: {
+    Chec
+  },
+  data() {
+    return {
+      dela: [
+        { id: 1, text: 'Сделать дз' },
+        { id: 2, text: 'Покушать' },
+        { id: 3, text: 'Налить чай' },
+        { id: 4, text: 'Погулять' }
+      ],
+    }
+  },
+  methods: {
+    addDela(newDelas) {
+      this.dela.unshift({
+        id: Date.now(),
+        text: newDelas
+      })
+    },
+
+    delit(id) {
+      this.dela = this.dela.filter(dela => dela.id !== id)
+    }
+  }
+}
+</script>
+<style></style>
---
+++ add/src/components/Chec.vue
@@ -0,0 +1,44 @@
+<template >
+    <div>
+        <input type="text" v-model="newDelas">
+        <button @click="addDel">Добавить</button>
+        <ul>
+            <li v-for="delas in dela" 
+            :key="delas.id">
+                <p :style="{ 'text-decoration': delas.checked ? 'line-through' : 'none' }">
+                    {{ delas.text }}
+                </p>
+                <input type="checkbox" v-model="delas.checked">
+                <button @click="delitDels(delas.id)">Удалить</button>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Chec',
+    props: {
+        dela: Object,
+        newDelas: String
+        
+    },
+    emits: ['addDela', 'delit','editDela'],
+    data() {
+        return {
+            newDelas: '',
+        }
+    },
+    methods: {
+        editDel(delas){
+            this.$emit('addDela', delas)
+        },
+        delitDels(id) {
+            this.$emit('delit', id)
+        },
+        addDel() {
+            this.$emit('addDela', this.newDelas)
+        }
+    }
+}
+</script>
+<style></style>
